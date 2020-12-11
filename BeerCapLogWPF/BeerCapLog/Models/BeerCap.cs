d466fd2585f2dcb695dd9607abcfa8b20d5f950b
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-12-11 11:25:00 -0500

    Models and DataAccess

---
+++ BeerCapLogWPF/BeerCapLog/Models/BeerCap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerCapLog.Models
+{
+    public class BeerCap
+    {
+        #region Properties
+        /// <summary>
+        /// The path to this Cap's Image.
+        /// </summary>
+        public string CapImagePath { get; set; }
+        /// <summary>
+        /// The brand this Cap belongs to.
+        /// </summary>
+        public Brand CapBrand { get; set; }
+        /// <summary>
+        /// The condition this cap is in.
+        /// </summary>
+        public Quality CapQuality { get; set; }
+        /// <summary>
+        /// The primary color of this cap.
+        /// </summary>
+        public Color PrimaryCapColor { get; set; }
+        /// <summary>
+        /// The secondary color of this cap.
+        /// </summary>
+        public Color SecondaryCapColor { get; set; }
+        /// <summary>
+        /// The message that appears under this cap.
+        /// </summary>
+        public string UnderCapMessage { get; set; }
+        #endregion
+
+        public BeerCap()
+        {
+            PrimaryCapColor = Color.FromName("White");
+        }
+    }
+}
---
+++ BeerCapLogWPF/BeerCapLog/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerCapLog.Models
+{
+    public class Brand
+    {
+        /// <summary>
+        /// The name of the brand.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
